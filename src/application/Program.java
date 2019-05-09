4c081d5168cd3f4f545a1633f80fb12e0bfaf567
Paulo José Maciel de Brito <paulojmbrito27@gmail.com>
2019-05-09 19:49:24 -0300

    Primeira Classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import tabuleiro.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Position pos = new Position(2, 5);
+		System.out.println(pos);
 
 	}
 
