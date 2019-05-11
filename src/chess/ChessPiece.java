f7a00437ae1c00280540b08d547166b253cb0c1e
Paulo José Maciel de Brito <paulojmbrito27@gmail.com>
2019-05-11 10:24:52 -0300

    Placing pieces on the board

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,31 +6,15 @@ import tabuleiro.Piece;
 public class ChessPiece extends Piece{
 	
 	private Color color;
-	private int moveCount;
 	
-	public ChessPiece(Board board, Color color, int moveCount) {
+	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
-		this.moveCount = moveCount;
 	}
 
 	public Color getColor() {
 		return color;
 	}
 
-
-	public int getMoveCount() {
-		return moveCount;
-	}
-
-	public void setMoveCount(int moveCount) {
-		this.moveCount = moveCount;
-	}
-	
-	
-	
-	
-	
-	
 
 }
